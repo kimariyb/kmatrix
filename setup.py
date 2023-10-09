9124328645de22aee6c39cecdc7e901c2540563d
kimariyb <kimariyb@163.com>
2023-10-09 17:42:41 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+setup.py
+
+This file is used to configure the setuptools package
+
+@author:
+Kimariyb, Hsiun Ryan (kimariyb@163.com)
+
+@address:
+XiaMen University, School of electronic science and engineering
+
+@license:
+Licensed under the MIT License.
+For details, see the LICENSE file.
+
+@data:
+2023-10-09
+"""
+
 import setuptools
 
 import matrix as ma
@@ -13,9 +33,15 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url=ma.__website__,
+    license='MIT',
+    keywords='matrix',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        'Development Status :: 1 - Alpha',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
