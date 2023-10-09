4bad6d2a32ffa434de6f485ebe9d1bffa6ffccc4
kimariyb <kimariyb@163.com>
2023-10-09 22:46:10 +0800

    create test package

---
+++ test/test.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+test.py
+
+This file is part of the test module.
+
+@author:
+Kimariyb, Hsiun Ryan (kimariyb@163.com)
+
+@address:
+XiaMen University, School of electronic science and engineering
+
+@license:
+Licensed under the MIT License.
+For details, see the LICENSE file.
+
+@data:
+2023-10-09
+"""
+
+from ..kmatrix import *
+
+# This is the test case for the matrix and the vector.
+_test_matrix_1 = Matrix([
+    [],
+    [],
+    ...
+])
+_test_matrix_2 = Matrix([
+    [],
+    [],
+    ...
+])
+_test_vector_row = Vector([
+    [],
+])
+_test_vector_col = Vector([
+    [],
+    [],
+    ...
+])
+    
+if __name__ == '__main__':
+    ...
